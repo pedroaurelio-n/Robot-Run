db06044c9fe1e1864e9e8c875932d0fcfc2499dc
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2022-11-22 19:43:07 -0300

    Main Menu overhaul. Added custom font and UI asset pack into project

---
+++ Assets/_src/Scripts/UI/Menu/AudioButton.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+namespace PedroAurelio.HermitCrab
+{
+    [RequireComponent(typeof(Button))]
+    public class AudioButton : MonoBehaviour
+    {
+        [Header("Sprites")]
+        [SerializeField] private Button oppositeButton;
+
+        [Header("Audio")]
+        [SerializeField] private AudioMixer mixer;
+        [SerializeField] private float volumeValue;
+        [SerializeField] private string volumeName = "SfxVolume";
+
+        public void TriggerVolume()
+        {
+            mixer.SetFloat(volumeName, Mathf.Log10(volumeValue) * 20f);
+
+            mixer.GetFloat(volumeName, out float vol);
+            Debug.Log(vol);
+
+            oppositeButton.gameObject.SetActive(true);
+            gameObject.SetActive(false);
+        }
+
+        private void OnEnable()
+        {
+            mixer.GetFloat(volumeName, out float vol);
+
+            if (Mathf.Pow(10, vol / 20f) == volumeValue)
+                gameObject.SetActive(false);
+            else
+                gameObject.SetActive(true);
+        }
+    }
+}
